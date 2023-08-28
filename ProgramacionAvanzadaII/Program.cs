cb670ea2425f9d92b9fcd2f9dd8c46ac3a52d4af
Matias Hernan Maglio <matias.maglio@tipsalud.com.ar>
2023-08-28 18:22:49 -0300

    Primer commit

---
+++ ProgramacionAvanzadaII/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgramacionAvanzadaII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
